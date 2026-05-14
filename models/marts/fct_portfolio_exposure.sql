6f3494c13ce0a99380bba85e4337ad2bc41679ba
Oussama Amara <oussama.amara@mscadataconsulting.com>
2026-05-14 00:04:19 -0600

    Add exposure percentage metric

--- models/marts/fct_portfolio_exposure.sql
+++ models/marts/fct_portfolio_exposure.sql
@@ -2,6 +2,13 @@ select
     portfolio_id,
     security_id,
     as_of_date,
-    sum(position_value) as total_position_value
+    sum(position_value) as total_position_value,
+    sum(position_value)
+        / sum(sum(position_value))
+            over(partition by portfolio_id)
+        as exposure_pct
 from {{ ref('int_position_valuations') }}
-group by 1, 2, 3
+group by
+    portfolio_id,
+    security_id,
+    as_of_date

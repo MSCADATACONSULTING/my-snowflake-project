71e0c939e0c35714f827e35e9aba70d36f9b7fc7
Oussama Amara <oussama.amara@mscadataconsulting.com>
2026-05-14 00:01:19 -0600

    Add sector exposure calculation

--- models/marts/fct_portfolio_exposure.sql
+++ models/marts/fct_portfolio_exposure.sql
@@ -1,7 +1,12 @@
 select
     portfolio_id,
     security_id,
+    sector,
     as_of_date,
     sum(position_value) as total_position_value
 from {{ ref('int_position_valuations') }}
-group by 1, 2, 3
+group by
+    portfolio_id,
+    security_id,
+    sector,
+    as_of_date
